fd177436391c5a73dedb2c8df6f8006cd92de9b3
Marcelo Gomes <marcelogomesrp@gmail.com>
2017-05-06 10:39:43 -0300

    public nas classe
    public

--- AdmCondominio/AdmCondominio/model/Condominio.cs
+++ AdmCondominio/AdmCondominio/model/Condominio.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace AdmCondominio.model
 {
-    class Condominio
+    public class Condominio
     {
     }
 }
--- AdmCondominio/AdmCondominio/model/Proprietario.cs
+++ AdmCondominio/AdmCondominio/model/Proprietario.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace AdmCondominio.model
 {
-    class Proprietario
+    public class Proprietario
     {
     }
 }
