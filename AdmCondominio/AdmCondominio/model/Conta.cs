6346fd561c31cea5bacd8b6cd674e9ece47d4e7d
maluf2017 <maluf.ricmal@gmail.com>
2017-05-06 10:56:45 -0300

    Conta
    ContaCondominio

---
+++ AdmCondominio/AdmCondominio/model/Conta.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdmCondominio.model
+{
+    public class Conta
+    {
+        public DateTime dataVencimento;
+        public DateTime dataPagamento;
+        public Decimal valordevido;
+        public Decimal valorfatura;
+        public String descricaofatura;
+    }
+}
