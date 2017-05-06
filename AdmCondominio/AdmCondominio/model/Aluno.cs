1d46c9147b05147dbb6188e49d2a884c2be37376
Marcelo Gomes <marcelogomesrp@gmail.com>
2017-05-06 11:42:17 -0300

    Criado teste
    TesteUNIT adicionado ao projeto como exemplo.

---
+++ AdmCondominio/AdmCondominio/model/Aluno.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdmCondominio.model
+{
+    public class Aluno
+    {
+        public String Nome { get; set; }
+        public Decimal NotaB1 { get; set; }
+        public Decimal NotaB2 { get; set; }
+        public Decimal Media
+        {
+            get { return (NotaB1 + NotaB2) / 2; }
+        }
+        public Boolean IsAprovado
+        {
+            get { return Media == 5; }
+        }
+    }
+}
---
+++ AdmCondominio/TesteProjeto/AlunoUnitTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AdmCondominio.model;
+
+namespace TesteProjeto
+{
+    [TestClass]
+    public class AlunoUnitTest
+    {
+        private Aluno criaAlunoBom()
+        {
+            return new Aluno()
+            {
+                Nome = "Maluf",
+                NotaB1 = 10,
+                NotaB2 = 10
+            };
+        }
+
+        [TestMethod]
+        public void AlunoBomMediaDezTest()
+        {
+            Aluno aluno = criaAlunoBom();
+            Assert.AreEqual(10, aluno.Media);
+        }
+        [TestMethod]
+        public void AlunoBomMediaNaoEhCinco()
+        {
+            Aluno aluno = criaAlunoBom();
+            Assert.AreNotEqual(5, aluno.Media);
+        }
+        [TestMethod]
+        public void AlunoBomIsAprovado()
+        {
+            Aluno aluno = criaAlunoBom();
+            Assert.IsTrue(aluno.IsAprovado);
+        }
+
+
+    }
+}
