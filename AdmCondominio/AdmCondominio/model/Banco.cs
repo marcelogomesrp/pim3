00d4700f094bf25b39458aab5e3ba98dcd0f56fa
Marcelo Gomes <marcelogomesrp@gmail.com>
2017-05-06 11:19:53 -0300

    Add varias coisas
    asdf

--- AdmCondominio/AdmCondominio/model/Banco.cs
+++ AdmCondominio/AdmCondominio/model/Banco.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace AdmCondominio.model
 {
-    class Banco
+    public class Banco
     {
     }
 }
--- AdmCondominio/AdmConodominioWinForms/Form1.cs
+++ AdmCondominio/AdmConodominioWinForms/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AdmCondominio.model;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,12 @@ namespace AdmConodominioWinForms
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Banco bd = new Banco();
+           
         }
     }
 }
